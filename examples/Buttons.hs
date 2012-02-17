328cf460887a84b44365eff7de2fe65ae9689948
Chris Done <chrisdone@gmail.com>
2012-02-17 16:56:24 +0100

    Update examples as argument order has changed to something more sane.

--- examples/Buttons.hs
+++ examples/Buttons.hs
@@ -10,7 +10,12 @@ import Graphics.UI.Ji
 
 -- | Main entry point. Starts a ji server.
 main :: IO ()
-main = serve 10001 runJi worker
+main = serve Config
+    { jiPort = 10001
+    , jiRun = runJi
+    , jiWorker = worker
+    , jiStatic = "wwwroot"
+    }
 
 -- | A per-user worker thread. Each user session has a thread.
 worker :: MonadJi m => m ()
@@ -18,80 +23,84 @@ worker = do
   setTitle "Buttons"
   body <- getElementByTagName "body"
   whenJust body $ \body -> do
-    setStyle body [("background","#333")]
+    setStyle [("background","#333")] body
     greet body
     makeButtons body
   handleEvents
 
 greet :: MonadJi m => Element -> m ()
 greet body = do
-  setStyle body [("color","#ccc")]
+  setStyle [("color","#ccc")] body
   
   header <- newElement "h2"
-  append body header
-  setText header "Hello, Haskell!"
+  appendTo body header
+  setText "Hello, Haskell!" header
   
   p <- newElementText body "p" ""
   vex <- link "https://github.com/chrisdone/ji/blob/master/examples/Buttons.hs"
               "Buttons.hs"
-  append p vex
+  appendTo p vex
 
   greeting <- newElement "div"
-  setText greeting "Try the buttons below, they hover and click."
-  append body greeting
+  setText "Try the buttons below, they hover and click." greeting
+  appendTo body greeting
+  return ()
 
 makeButtons :: MonadJi m => Element -> m ()
 makeButtons body = do
   list <- newElement "ul"
-  setStyle list [("color","#aaa")]
+  setStyle [("color","#aaa")] list
   
-  button1 <- appendButton body button1Title
-  button2 <- appendButton body button2Title
-  append body list
+  button1 <- appendToButton body button1Title
+  button2 <- appendToButton body button2Title
+  appendTo body list
     
-  onHover button1 $ \_ -> do setText button1 (button1Title ++ " [hover]")
-  onBlur button1  $ \_ -> do setText button1 button1Title
+  onHover button1 $ \_ -> do setText (button1Title ++ " [hover]") button1; return ()
+  onBlur button1  $ \_ -> do setText button1Title button1; return ()
   onClick button1 $ \_ -> do
     li <- newElement "li"
     io $ threadDelay $ 1000 * 1000 * 1
-    setText button1 (button1Title ++ " [pressed]")
-    setHtml li "<b>Delayed</b> result!"
-    append list li
+    setText (button1Title ++ " [pressed]")  button1
+    setHtml "<b>Delayed</b> result!" li
+    appendTo list li
+    return ()
 
-  onHover button2 $ \_ -> do setText button2 (button2Title ++ " [hover]")
-  onBlur button2  $ \_ -> do setText button2 button2Title
+  onHover button2 $ \_ -> do setText (button2Title ++ " [hover]") button2; return ()
+  onBlur button2  $ \_ -> do setText button2Title button2; return ()
   onClick button2 $ \_ -> do
     li <- newElement "li"
-    setText button2 (button2Title ++ " [pressed]")
-    setHtml li "Zap! Quick result!"
-    append list li
+    setText (button2Title ++ " [pressed]") button2
+    setHtml "Zap! Quick result!" li
+    appendTo list li
+    return ()
 
   where button1Title = "Click me, I delay a bit"
         button2Title = "Click me, I work immediately"
 
-appendButton :: MonadJi m => Element -> String -> m Element
-appendButton body caption = do
+appendToButton :: MonadJi m => Element -> String -> m Element
+appendToButton body caption = do
   p <- newElement "p"
   button <- newElement "a"
-  append body p
-  append p button
-  setText button caption
-  setStyle button [("cursor","pointer")
-                  ,("color","#acc2a1")
-                  ,("text-decoration","underline")]
+  appendTo body p
+  appendTo p button
+  setText caption button
+  setStyle [("cursor","pointer")
+           ,("color","#acc2a1")
+           ,("text-decoration","underline")]
+           button
   return button
 
 link :: MonadJi m => String -> String -> m Element
 link url text = do
   el <- newElement "a"
-  setAttr el "href" url
-  setText el text
-  setStyle el [("color","#acc2a1")]
+  setAttr "href" url el
+  setText text el
+  setStyle [("color","#acc2a1")] el
   return el
 
 newElementText :: MonadJi m => Element -> String -> String -> m Element
 newElementText parent tagName text = do
   el <- newElement tagName
-  append parent el
-  setText el text
+  appendTo parent el
+  setText text el
   return el
