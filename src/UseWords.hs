33c2bc262e7e5304a9f075278f759b02581683d0
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2013-04-26 11:42:37 +0200

    Update 'UseWords' example to new API.

--- src/UseWords.hs
+++ src/UseWords.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE ViewPatterns, PackageImports #-}
+{-# LANGUAGE CPP, PackageImports #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Main where
 
@@ -10,70 +11,79 @@ import Control.Monad.IO
 import Data.Maybe
 #ifdef CABAL
 import "threepenny-gui" Graphics.UI.Threepenny
-import "threepenny-gui" Graphics.UI.Threepenny.DOM
-import "threepenny-gui" Graphics.UI.Threepenny.Elements
-import "threepenny-gui" Graphics.UI.Threepenny.JQuery
+import "threepenny-gui" Graphics.UI.Threepenny.Elements as H
 #else
 import Graphics.UI.Threepenny
-import Graphics.UI.Threepenny.DOM
-import Graphics.UI.Threepenny.Elements
-import Graphics.UI.Threepenny.JQuery
+import Graphics.UI.Threepenny.Elements as H
 #endif
 import Text.Parsec
 
+
 main :: IO ()
-main = serve Config
-    { tpPort = 10003
-    , tpRun = runTP
-    , tpWorker = worker
-    , tpInitHTML = "use-words.html"
-    , tpStatic = "wwwroot"
-    }
+main = startGUI Config
+    { tpPort       = 10000
+    , tpCustomHTML = Nothing
+    , tpStatic     = "../wwwroot"
+    } setup
 
-worker :: TP ()
-worker = do
-  andthen <- io (readFile fname)
-  case parts fname andthen of
-    Left parseerror -> debug (show parseerror)
-    Right parts -> do
-      body <- getBody
-      wrap <- new # setClass "wrap" # addTo body
-      codeLink wrap
-      heading <- new # setClass "header" # addTo wrap
-      ul <- new # setClass "vars" # addTo wrap
-      let (header,drop 2 -> rest) = splitAt 3 parts
-      r <- liftM catMaybes $ forM header (addPart heading)
-      rs <- liftM catMaybes $ forM rest (addPart wrap)
-      forM_ vars (addVarChoice ul (r ++ rs))
-      handleEvents
 
-addVarChoice ul refs (label,(name,def)) = do
-  li <- new # addTo ul
-  newLabel # addTo li # setText (label ++ ":") # unit
-  input <- newInput # addTo li # set "value" def # setClass "var-value"
-  bind "livechange" input $ \(EventData (concat . catMaybes -> text)) ->
-    forM_ (filter ((==name).fst) refs) $ \(_,el) -> do
-      setText text el
+filename = "../wwwroot/and-then-haskell.txt"
 
-addPart wrap part = do
-  case part of
-    Text str -> do
-      new # addTo wrap # setClass "text" # setText str # unit
-      return Nothing
-    Ref var -> do
-      v <- new # addTo wrap
-               # setClass "var"
-               # maybe (setText ("{" ++ var ++ "}"))
-                       (either setHtml setText)
-                       (lookup var templatevars)
-      return (Just (var,v))
+setup :: Window -> IO ()
+setup w = do
+    andthen <- readFile filename
+    case parts filename andthen of
+        Left parseerror -> debug w $ show parseerror
+        Right parts     -> do
+            body    <- getBody w
+            addStyleSheet w "use-words.css"
 
-codeLink wrap = do
-  newAnchor # set "href" "https://github.com/chrisdone/ji/blob/master/examples/UseWords.hs"
-            # setText "View source code"
-            # setClass "code-link"
-            # addTo wrap
-            # unit
+            wrap    <- new w # set cssClass "wrap"   # appendTo body
+            codeLink w wrap
+            
+            heading <- new w # set cssClass "header" # appendTo wrap
+            ul      <- new w # set cssClass "vars"   # appendTo wrap
+            
+            let (header, Prelude.drop 2 -> rest) = splitAt 3 parts
+            r  <- liftM catMaybes $ forM header (addPart w heading)
+            rs <- liftM catMaybes $ forM rest   (addPart w wrap)
+            forM_ vars (addVarChoice w ul (r ++ rs))
+
+addVarChoice w ul refs (label,(name,def)) = do
+    li <- new w # appendTo ul
+    H.span w
+        # set text (label ++ ":")
+        # appendTo li
+    input <- H.input w
+        # set cssClass "var-value"
+        # set value def
+        # appendTo li
+    
+    on (domEvent "livechange") input $ \(EventData xs) -> do
+        let s = concat $ catMaybes xs
+        forM_ (filter ((==name).fst) refs) $ \(_,el) -> do
+            element el # set text s
+
+addPart w wrap (Text str) = do
+    new w
+        # set cssClass "text"
+        # set text str
+        # appendTo wrap
+    return Nothing
+addPart w wrap (Ref  var) = do
+    v <- new w
+        # set cssClass "var"
+        # maybe (set text $ "{" ++ var ++ "}") (either (set html) (set text))
+                (lookup var templatevars)
+        # appendTo wrap
+    return (Just (var,v))
+
+codeLink w wrap = do
+    anchor w
+        # set (attr "href") "https://github.com/HeinrichApfelmus/threepenny-gui/blob/master/src/UseWords.hs"
+        # set text "View source code"
+        # set cssClass "code-link"
+        # appendTo wrap
 
 fname = "wwwroot/and-then-haskell.txt"
 
@@ -88,5 +98,5 @@ data Part = Text String | Ref String deriving Show
 
 parts :: SourceName -> String -> Either ParseError [Part]
 parts = parse (many (ref <|> text)) where
-  text = Text <$> many1 (notFollowedBy (string "{") *> anyChar)
-  ref = Ref <$> (string "{" *> many1 (noneOf "}") <* (string "}"))
+    text = Text <$> many1 (notFollowedBy (string "{") *> anyChar)
+    ref  = Ref  <$> (string "{" *> many1 (noneOf "}") <* (string "}"))
