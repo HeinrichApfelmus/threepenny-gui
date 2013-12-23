de72d95a02fa99594b52f8f3cdf572c15cc4f979
Ken Friis Larsen <ken@friislarsen.net>
2013-12-23 12:17:32 +0100

    Add canvas functions `fillRect` and `setFillStyle`

--- src/Graphics/UI/Threepenny/Canvas.hs
+++ src/Graphics/UI/Threepenny/Canvas.hs
@@ -1,10 +1,11 @@
 module Graphics.UI.Threepenny.Canvas (
     -- * Synopsis
     -- | Partial binding to the HTML5 canvas API.
-    
+
     -- * Documentation
     Canvas,
-    Vector, drawImage, clearCanvas,
+    Vector, drawImage, clearCanvas
+    , fillRect, setFillStyle
     ) where
 
 import Graphics.UI.Threepenny.Core
@@ -26,3 +27,12 @@ clearCanvas :: Canvas -> UI ()
 clearCanvas = runFunction . ffi "%1.getContext('2d').clear()"
 
 
+-- | Draw a filled rectangle (using the current fillStyle) at position (x, y) with width w and height h.
+fillRect :: Int -> Int -> Int -> Int -> Canvas -> UI()
+fillRect x y w h canvas =
+  runFunction $ ffi "%1.getContext('2d').fillRect(%2, %3, %4, %5)" canvas x y w h
+
+-- | Set the color or style to use inside shapes. Default #000 (black).
+setFillStyle :: String -> Canvas -> UI()
+setFillStyle sty canvas =
+  runFunction $ ffi "%1.getContext('2d').fillStyle = %2" canvas sty
