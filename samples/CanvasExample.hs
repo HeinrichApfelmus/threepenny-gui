232b138462d2b30694e6b3326796eaf56d26bafd
Ken Friis Larsen <ken@friislarsen.net>
2013-12-23 15:37:38 +0100

    Update CanvasExample to use the newly added functions

--- samples/CanvasExample.hs
+++ samples/CanvasExample.hs
@@ -18,22 +18,47 @@ setup window = do
         # set UI.height 400
         # set UI.width  400
         # set style [("border", "solid black 1px")]
+
+    addRects <- UI.button #+ [string "Add some rectangles."]
+    addArcs <- UI.button #+ [string "Add some arcs and circles."]
     clear  <- UI.button #+ [string "Clear the canvas."]
 
     getBody window #+ [column
-        [element canvas, string "Click to see some rectangles"]
-        ,element clear
+        [element canvas]
+        ,element addRects, element addArcs, element clear
         ]
 
     let rects = [(75,100, 10, 10, "blue"), (325, 100, 15, 15, "blue"),
                  (200, 200, 25, 40, "orange"),
                  (250, 300, 75, 15, "plum")]
-
     let drawRect (x,y,w,h,col) = do
           UI.setFillStyle col canvas
           UI.fillRect x y w h canvas
 
-    on UI.click canvas $ const $ forM_ rects drawRect
+    on UI.click addRects $ const $ forM_ rects drawRect
+
+    let circles = [ (200, 200, 25, "orange")
+                  , (275, 200, 25, "plum")
+                  , (55, 200, 25, "plum") ]
+    let drawCircle (x,y,r,col) = do
+          UI.setFillStyle col canvas
+          UI.beginPath canvas
+          UI.arc (x,y) r 0 (ceiling $ 2*pi) canvas
+          UI.fill canvas
+
+    let slices = [ (325, 115, 25, -1, 0, "lightblue") ]
+    let drawSlice (x,y,r,start,end,col) = do
+          UI.setFillStyle col canvas
+          UI.beginPath canvas
+          UI.arc (x,y) r start end canvas
+          UI.lineTo (x,y) canvas
+          UI.stroke canvas
+          UI.closePath canvas
+          UI.fill canvas
+
+    on UI.click addArcs $ const $ do
+      forM_ circles drawCircle
+      forM_ slices drawSlice
 
     on UI.click clear  $ const $ do
         UI.clearCanvas canvas
