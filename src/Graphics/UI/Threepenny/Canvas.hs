02a53505bcf5e9547b3950f981e8e4d6035d8f3f
Mathieu Suen <mathk.sue@gmail.com>
2014-06-26 16:54:44 +0200

    Add the stroke style and stroke rect function

--- src/Graphics/UI/Threepenny/Canvas.hs
+++ src/Graphics/UI/Threepenny/Canvas.hs
@@ -4,7 +4,7 @@ module Graphics.UI.Threepenny.Canvas (
     
     -- * Documentation
     Canvas,
-    Vector, fillStyle, fillRect, drawImage, clearCanvas,
+    Vector, fillStyle, fillRect, strokeStyle, strokeRect, drawImage, clearCanvas,
     ) where
 
 import Graphics.UI.Threepenny.Core
@@ -17,6 +17,10 @@ type Canvas = Element
 type Vector = (Int,Int)
 
 -- | Set the fill style
+--
+-- Example of usage
+--
+-- > fillStyle "#CAB" canvas
 fillStyle :: String -> Canvas -> UI ()
 fillStyle style canvas =
     runFunction $ ffi "%1.getContext('2d').fillStyle = %2" canvas style
@@ -25,6 +29,20 @@ fillStyle style canvas =
 fillRect :: Vector -> Vector -> Canvas -> UI ()
 fillRect (x1, y1) (x2, y2) canvas =
     runFunction $ ffi "%1.getContext('2d').fillRect(%2,%3,%4,%5)" canvas x1 y1 x2 y2
+
+-- | Set the stroke style
+--
+-- Example of usage
+--
+-- > strokeStyle "#CAB" canvas
+strokeStyle :: String -> Canvas -> UI ()
+strokeStyle style canvas =
+    runFunction $ ffi "%1.getContext('2d').strokeStyle = %2" canvas style
+
+-- | Drow a rectangle with the current strokeStyle
+strokeRect :: Vector -> Int -> Int -> Canvas -> UI ()
+strokeRect (x, y) width height canvas =
+    runFunction $ ffi "%1.getContext('2d').strokeRect(%2,%3,%4,%5)" canvas x y width height
 
 -- | Draw the image of an image element onto the canvas at a specified position.
 drawImage :: Element -> Vector -> Canvas -> UI ()
