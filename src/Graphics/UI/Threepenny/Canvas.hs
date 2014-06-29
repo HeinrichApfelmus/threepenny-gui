f24b7908297d61970b8702e683b95cee07c89152
Mathieu Suen <mathk.sue@gmail.com>
2014-06-29 12:04:32 +0200

    Revert "Add fill rect"
    This reverts commit 3f6b66c36a5c07c7d8a605f8e262b2dcaa8ea877.

--- src/Graphics/UI/Threepenny/Canvas.hs
+++ src/Graphics/UI/Threepenny/Canvas.hs
@@ -4,7 +4,7 @@ module Graphics.UI.Threepenny.Canvas (
     
     -- * Documentation
     Canvas,
-    Vector, fillRect, drawImage, clearCanvas,
+    Vector, drawImage, clearCanvas,
     ) where
 
 import Graphics.UI.Threepenny.Core
@@ -15,11 +15,6 @@ import Graphics.UI.Threepenny.Core
 type Canvas = Element
 
 type Vector = (Int,Int)
-
--- | Draw a rectangle filled with the current fill style
-fillRect :: Vector -> Vector -> Canvas -> UI ()
-fillRect (x1, y1) (x2, y2) canvas =
-    runFunction $ ffi "%1.getContext('2d').fillRect(%2,%3,%4,%5)" canvas x1 y1 x2 y2
 
 -- | Draw the image of an image element onto the canvas at a specified position.
 drawImage :: Element -> Vector -> Canvas -> UI ()
