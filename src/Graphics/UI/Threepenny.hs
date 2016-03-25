c0bd1455f8d9ae697eb153744ef97bf0acc4ed3a
Gabor Greif <ggreif@gmail.com>
2016-03-25 10:13:52 +0100

    jsPort is Maybe Int

--- src/Graphics/UI/Threepenny.hs
+++ src/Graphics/UI/Threepenny.hs
@@ -60,7 +60,7 @@ Additional static content is served from the @../wwwroot@ directory.
 > main :: IO ()
 > main = do
 >     startGUI defaultConfig
->         { jsPort       = 8023
+>         { jsPort       = Just 8023
 >         , jsStatic     = Just "../wwwroot"
 >         } setup
 
