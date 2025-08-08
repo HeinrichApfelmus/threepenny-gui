d2fae047e5b63f7eb548c19655898adafffbf4cb
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2025-08-08 20:07:45 +0200

    Use `-tenvironment` instead of a custom `target.conf`

--- lib/javascript-ffi-threepenny/build-microhs.sh
+++ lib/javascript-ffi-threepenny/build-microhs.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-mhs -tweb -i./src Foreign.TestJavaScript -oTest.js
+
+# Use variables to avoid adding stuff to target.conf for now
+export CC=emcc
+export MHSCCFLAGS="-O3 -sEXPORTED_FUNCTIONS=_apply_sp,_main -sEXPORTED_RUNTIME_METHODS=stringToNewUTF8 -sALLOW_MEMORY_GROWTH -sTOTAL_STACK=5MB -sSINGLE_FILE -DUSE_SYSTEM_RAW -Wno-address-of-packed-member"
+export MHSCCLIBS="-lm"
+export MHSCONF="unix"
+
+mhs -tenvironment -i./src Foreign.TestJavaScript -oTest.js
+# mhs -ddump-combinator -i./src Foreign.TestJavaScript
